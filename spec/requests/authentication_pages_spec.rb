0387a684702b4bfb7d9f798f6cef71422d594713
Ryan Teo <ryan.teo@gmail.com>
2013-03-03 08:28:12 +0800

    Update authentication to use form_tag

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -19,7 +19,8 @@ describe "Authentication" do
 
       describe "after visiting another page" do
         before { click_link "Home"}
-        it { should_not have_selector('div.alert.alert-error')}
+        # it { should_not have_selector('div.alert.alert-error')}
+        it { should_not have_error_message('')}
       end
     end
 
