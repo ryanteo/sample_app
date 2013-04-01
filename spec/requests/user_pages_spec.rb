405580ce48e1790148e8e55627abdafa98b60914
Ryan Teo <ryan.teo@gmail.com>
2013-04-01 08:55:55 +0800

    Finish user edit, update, index and destroy actions

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -3,6 +3,58 @@ require 'spec_helper'
 describe "UserPages" do
   subject { page }
 
+  # When visiting /users/, it should show a list of users (FactoryGirl to create 30 sample users)
+  describe "index" do
+    let(:user) { FactoryGirl.create(:user) }
+
+    before(:each) do
+      sign_in user
+      visit users_path
+    end
+
+    it { should have_selector("title", text: 'All users') }
+    it { should have_selector('h1', text: "All users") }
+
+    # Test for pagination, page should show 30 users
+    describe "pagination" do
+      before(:all)  { 30.times { FactoryGirl.create(:user) } }
+      after(:all)   { User.delete_all }
+
+      it { should have_selector('div.pagination') }
+
+      it "should list each user" do
+        User.paginate(page: 1).each do |user|
+          page.should have_selector('li', text: user.name)
+        end
+      end
+    end
+
+    # admin users should be able to delete users
+    describe "delete links" do
+      it { should_not have_link('delete') }
+
+      # Create an admin user and sign in
+      describe "as an admin user" do
+        let(:admin) { FactoryGirl.create(:admin) }
+        before do
+          sign_in admin
+          visit users_path
+        end
+        # Delete another non-admin user
+        it { should have_link('delete', href: user_path(User.first)) }
+        it 'should be able to delete another user' do
+          expect { click_link('delete') }.to change(User, :count).by(-1)
+        end
+
+        # admin should not be able to able to delete his own account
+        it { should_not have_link('delete', href: user_path(admin)) }
+      end
+    end
+  end
+
+
+
+
   describe "signup page" do
     before { visit signup_path }
     it { should have_selector('h1',     text: 'Sign up') }
@@ -56,4 +108,46 @@ describe "UserPages" do
 
     end
   end
+
+  
+  describe "edit" do
+    let(:user) { FactoryGirl.create(:user)}
+    before do
+      sign_in user  #   user must sign in before they can edit profile
+      visit edit_user_path(user)
+    end
+
+    describe "page" do 
+      it {should have_selector('h1', text: "Update your profile")}
+      it {should have_selector('title', text: "Edit user")}
+      it {should have_link('change', href: "http://gravatar.com/emails")}
+    end
+
+    # there should be an error message for invalid submission
+    describe "with invalid information" do
+      before { click_button "Save changes"} 
+
+      it { should have_content('error')}
+    end
+
+    describe "with valid information" do
+      let(:new_name)  {"New Name"}
+      let(:new_email) {"new@example.com"}
+      before do
+        fill_in "Name",           with: new_name
+        fill_in "Email",          with: new_email
+        fill_in "Password",       with: user.password
+        fill_in "Confirmation",   with: user.password
+        click_button "Save changes"
+      end
+
+      it { should have_selector('title', text: new_name)}
+      it { should have_selector('div.alert.alert-success')}
+      it { should have_link('Sign out', href: signout_path)}
+      specify { user.reload.name.should   == new_name }
+      specify { user.reload.email.should  == new_email}
+    end
+
+  end
+
 end
